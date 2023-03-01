1dbcdc0a3dc8919473d992c38bb8eb19120e4668
MRiivik <mriivik@tktk.ee>
2023-03-01 20:55:19 +0200

    add Week7Functions code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine(); // kasutaja annab argumendi väärtusele 'userInput'
+
+PrintAnyWord(userInput);
+
+static void PrintAnyWord(string anyString) // anyString = parameeter
+{
+	anyString= anyString.ToUpper();
+	for (int i = 0; i < 5; i++)
+	{
+		Console.WriteLine(anyString);
+	}
+}
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodbye(); // toob allolevad käsud välja
+}
+static void PrintHello() //function ehk 'meetod'
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later, alligator!");
+}
